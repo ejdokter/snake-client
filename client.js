8a02d98c6ba1e1658e44b2777f9201af225783cb
Eric Dokter <edokter11@gmail.com>
2022-02-21 13:35:36 -0700

    name reply to server

--- client.js
+++ client.js
@@ -12,6 +12,11 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log(data)
   })
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server')
+    conn.write('Name: EJD')
+  })
   return conn
 }
 
