5d1c9aa6ee62bfda0e3e0fb5d1e13fdcaed48257
Eric Dokter <edokter11@gmail.com>
2022-02-23 09:58:41 -0700

    create constants file and reformated

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = 'localhost'
+const PORT = '50541'
+
+const input = {
+  'w': 'Move: up',
+  'd': 'Move: right',
+  's': 'Move: down',
+  'a': 'Move: left',
+  'x': 'Say: nice!',
+  'z': 'Say: good game'
+}
+
+module.exports = {
+  IP,
+  PORT,
+  input
+}
--- input.js
+++ input.js
@@ -1,4 +1,6 @@
 // setup interface to handle user input from stdin
+const { input } = require('./constants')
+
 let connection;
 
 const setupInput = function (conn) {
@@ -16,24 +18,25 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit()
   }
-  if (key === 'w') {
-    connection.write('Move: up')
-  }
-  if (key === 'a') {
-    connection.write('Move: left')
-  }
-  if (key === 's') {
-    connection.write('Move: down')
-  }
-  if (key === 'd') {
-    connection.write('Move: right')
-  }
-  if (key === 'x') {
-    connection.write('Say: nice!')
-  }
-  if (key === 'z') {
-    connection.write('Say: good game')
-  }
+  connection.write(input[key])
+  // if (key === 'w') {
+  //   connection.write('Move: up')
+  // }
+  // if (key === 'a') {
+  //   connection.write('Move: left')
+  // }
+  // if (key === 's') {
+  //   connection.write('Move: down')
+  // }
+  // if (key === 'd') {
+  //   connection.write('Move: right')
+  // }
+  // if (key === 'x') {
+  //   connection.write('Say: nice!')
+  // }
+  // if (key === 'z') {
+  //   connection.write('Say: good game')
+  // }
 }
 
 module.exports = setupInput
